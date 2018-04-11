23c45e6210aa2312f0306adf64d394c4498b0b8d
Derrick Godwin <jgodwin3@stu.jsu.edu>
2018-04-11 13:08:56 -0500

    20% tests passing for Feature 3

--- Tas_Sp2018/src/tas_sp2018/Punch.java
+++ Tas_Sp2018/src/tas_sp2018/Punch.java
@@ -17,6 +17,7 @@ public class Punch {
     private String sdf;
     private int eventtypeid;
     private int punchTypeId;
+    private String eventData;
     
 
     public Punch(int terminalId, String badgeId, int shiftId, long originalts, int eventtypeid) {
@@ -35,7 +36,7 @@ public class Punch {
         sdf = new SimpleDateFormat("EEE MM/dd/YYYY HH:mm:ss").format(original.getTime()).toUpperCase();
     }
 	
-	public Punch(String badgeId, int terminalId, int punchTypeId, long timeStamp){
+    public Punch(String badgeId, int terminalId, int punchTypeId, long timeStamp){
 
         this.badgeId = badgeId;
         this.terminalId = terminalId;
@@ -50,12 +51,111 @@ public class Punch {
         sdf = new SimpleDateFormat("EEE MM/dd/YYYY HH:mm:ss").format(original.getTime()).toUpperCase();
              
       //sdf = new SimpleDateFormat("EEE MM/dd/YYYY HH:mm:ss").format(original.getTime()).toUpperCase();
-      
-      public void adjust (Shift s) {
+    }
+    
+    public void adjust (Shift s) {
           
-        GregorianCalendar lunchStart = new GregorianCalendar(original.get(Calendar.YEAR), original.get(Calendar.MONTH), original.get(Calendar.DAY_OF_MONTH), s.getLunchstart().getHours(), s.getLunchstart().getMinutes());
-        GregorianCalendar lunchStop = new GregorianCalendar(original.get(Calendar.YEAR), original.get(Calendar.MONTH), original.get(Calendar.DAY_OF_MONTH), s.getLunchstop().getHours(), s.getLunchstop().getMinutes());   
-  
+       //adjusted = original;
+       //eventData = "(Wrong)";
+       /*Each calendar represents the bounds for the critical zones*/
+       
+       s.getStart().set(Calendar.YEAR, original.get(Calendar.YEAR));
+       s.getStart().set(Calendar.MONTH, original.get(Calendar.MONTH));
+       s.getStart().set(Calendar.DAY_OF_MONTH, original.get(Calendar.DAY_OF_MONTH));
+       s.getStop().set(Calendar.YEAR, original.get(Calendar.YEAR));
+       s.getStop().set(Calendar.MONTH, original.get(Calendar.MONTH));
+       s.getStop().set(Calendar.DAY_OF_MONTH, original.get(Calendar.DAY_OF_MONTH));
+       s.getLunchstart().set(Calendar.YEAR, original.get(Calendar.YEAR));
+       s.getLunchstart().set(Calendar.MONTH, original.get(Calendar.MONTH));
+       s.getLunchstart().set(Calendar.DAY_OF_MONTH, original.get(Calendar.DAY_OF_MONTH));
+       s.getLunchstop().set(Calendar.YEAR, original.get(Calendar.YEAR));
+       s.getLunchstop().set(Calendar.MONTH, original.get(Calendar.MONTH));
+       s.getLunchstop().set(Calendar.DAY_OF_MONTH, original.get(Calendar.DAY_OF_MONTH));
+       
+       
+       GregorianCalendar beforeShiftStart = new GregorianCalendar();
+       beforeShiftStart.setTimeInMillis(s.getStart().getTimeInMillis());
+       //System.out.println(beforeShiftStart.getTime());
+       beforeShiftStart.add(Calendar.MINUTE, -(s.getInterval()));
+       //System.out.println(beforeShiftStart.getTime());
+       
+       GregorianCalendar beforeGraceStart = new GregorianCalendar();
+       beforeGraceStart.setTimeInMillis(s.getStart().getTimeInMillis());
+       beforeGraceStart.add(Calendar.MINUTE, s.getGracePeriod());
+       
+       GregorianCalendar afterStartDock = new GregorianCalendar();
+       afterStartDock.setTimeInMillis(s.getStart().getTimeInMillis());
+       afterStartDock.add(Calendar.MINUTE, s.getDock());
+       
+       GregorianCalendar beforeStopDock = new GregorianCalendar();
+       beforeStopDock.setTimeInMillis(s.getStart().getTimeInMillis());
+       beforeStopDock.add(Calendar.MINUTE, -( s.getDock() ) );
+       
+       GregorianCalendar beforeStopGrace = new GregorianCalendar();
+       beforeStopGrace.setTimeInMillis(s.getStart().getTimeInMillis());
+       beforeStopGrace.add(Calendar.MINUTE, -( s.getGracePeriod() ) );
+       
+       GregorianCalendar afterShiftStop = new GregorianCalendar();
+       afterShiftStop.setTimeInMillis(s.getStart().getTimeInMillis());
+       afterShiftStop.add(Calendar.MINUTE, s.getInterval());
+       
+      // System.out.println(original.getTime());
+       //System.out.println(beforeShiftStart.getTime());
+       //System.out.println(s.getStart().getTime());
+     
+       
+       int punchmins = original.get(Calendar.MINUTE);
+           
+       int diffmins = Math.abs(s.getInterval() - punchmins);
+       
+       if(original.before(s.getStart()) && original.after(beforeShiftStart)){
+           adjusted.setTimeInMillis(s.getStart().getTimeInMillis());
+           eventData = "(Shift Start)";
+       }
+       else if(original.after(s.getStart()) && original.before(beforeGraceStart)){
+           adjusted.setTimeInMillis(s.getStart().getTimeInMillis());
+           eventData = "(Shift Start)";
+       }
+       else if(original.after(beforeGraceStart) && original.before(afterStartDock)){
+           adjusted.setTimeInMillis(afterStartDock.getTimeInMillis());
+           eventData = "(Shift Dock)";
+       }
+       else if(original.after(s.getLunchstart()) && original.before(s.getLunchstop()) && eventtypeid == 0){
+           adjusted.setTimeInMillis(s.getLunchstart().getTimeInMillis());
+           eventData = "(Lunch Start)";
+       }
+       else if(original.after(s.getLunchstart()) && original.before(s.getLunchstop()) && eventtypeid == 1){
+           adjusted.setTimeInMillis(s.getLunchstop().getTimeInMillis());
+           eventData = "(Lunch Stop)";
+       }
+       else if(original.after(beforeStopDock) && original.before(beforeStopGrace)){
+           adjusted.setTimeInMillis(beforeStopDock.getTimeInMillis());
+           eventData = "(Shift Dock)";
+       }
+       else if(original.after(beforeStopGrace) && original.before(s.getStop())){
+           adjusted.setTimeInMillis(s.getStop().getTimeInMillis());
+           eventData = "(Shift Stop)";
+       }
+       else if(original.after(s.getStop()) && original.before(afterShiftStop)){
+           adjusted.setTimeInMillis(s.getStop().getTimeInMillis());
+           eventData = "(Shift Stop)";
+       }
+       else if(punchmins % s.getInterval() == 0){
+          adjusted.setTimeInMillis(original.getTimeInMillis());
+          eventData = "(None)";
+       }
+       else if( diffmins <= (s.getInterval() / 2) ){
+            adjusted.setTimeInMillis(original.getTimeInMillis());
+            adjusted.roll(Calendar.MINUTE, diffmins);
+            adjusted.set(Calendar.SECOND, 0);
+            eventData = "(Interval Round)";
+        }
+       else if(diffmins > (s.getInterval() / 2)){
+            adjusted.setTimeInMillis(original.getTimeInMillis());
+            adjusted.roll(Calendar.MINUTE, -diffmins);
+            adjusted.set(Calendar.SECOND, 0);
+            eventData = "(Interval Round)";
+        }
     }
       
     public String getPunchDescription() {
@@ -107,7 +207,30 @@ public class Punch {
 
         return eventtypeid;
     }
-public String printOriginalTimestamp() {
+
+    
+    public String printAdjustedTimestamp(){
+    
+        
+        String adj = new SimpleDateFormat("EEE MM/dd/YYYY HH:mm:ss").format(adjusted.getTime()).toUpperCase();
+     
+        String Status = "";
+
+       if (eventtypeid == 1) {
+            Status = " CLOCKED IN: ";
+
+        } else if (eventtypeid == 0) {
+            Status = " CLOCKED OUT: ";
+
+        } else {
+            Status = " TIMED OUT: ";
+        }
+
+        return "#" + badgeId + Status + adj + " " +eventData;
+    
+    }
+    
+    public String printOriginalTimestamp() {
 
         String Status = "";
 
