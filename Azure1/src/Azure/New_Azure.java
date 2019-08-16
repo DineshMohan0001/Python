6026d137d00cc013608826c37b5f57da75286d4e
DineshMohan0001 <53462648+DineshMohan0001@users.noreply.github.com>
2019-08-16 14:11:27 +0530

    new Azure project

---
+++ Azure1/src/Azure/New_Azure.java
@@ -0,0 +1,12 @@
+package Azure;
+
+public class New_Azure {
+
+	public static void main(String[] args) 
+	{
+		// TODO Auto-generated method stub
+		System.out.println("THIS is THE sample OUTPUT");
+
+	}
+
+}
